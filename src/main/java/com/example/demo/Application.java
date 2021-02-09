d55781ddd5cbbb9a4e1afc28d70a99c093b809ab
Khwilo Kabaka <khwilowatai@gmail.com>
2021-02-09 13:22:29 +0300

    Add section 1 of the project implementations

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
+import com.example.demo.student.StudentRepository;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 
+  @Bean
+  CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+    return args -> {
+      Student khwilo = new Student("Khwilo", "Kabaka", "khwilowatai@gmail.com", 27);
+      studentRepository.save(khwilo);
+    };
+  }
 }
